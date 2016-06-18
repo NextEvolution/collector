724dc8522ef7959ffd0a5e4ae0982ba64fc18c4b
Barry Williams <barry.e.williams42@gmail.com>
2016-06-18 17:27:15 -0600

    started to parse json for group data

--- facebookripper/facebookripper.go
+++ facebookripper/facebookripper.go
@@ -3,6 +3,9 @@ package facebookripper
 import (
 	"net/http"
 	"fmt"
+	fb "nextevolution/collector/facebookripper/fbobjects"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func NewFacebookRipper(url string) *FacebookRipper {
@@ -15,8 +18,20 @@ type FacebookRipper struct {
 	url string
 }
 
-func (f *FacebookRipper) LookForOrders(userId string, token string){
+func (f *FacebookRipper) GetUsersGroups(userId string, token string) []fb.Group {
+
 	url := fmt.Sprintf("%s/%s/groups?access_token=%s",f.url, userId, token)
-	fmt.Println("url: " + url)
-	http.Get(url)
+	resp,err := http.Get(url)
+
+	if err != nil {
+		//todo: handle this
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	envelope := fb.GroupEnvelope{}
+	json.Unmarshal(body, &envelope)
+
+	return envelope.Data
 }
--- facebookripper/facebookripper_test.go
+++ facebookripper/facebookripper_test.go
@@ -5,33 +5,33 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "./fakefacebook"
 	"github.com/onsi/gomega/ghttp"
-
-	"net/http"
+	"fmt"
 )
 
 var _ = Describe("Facebookripper", func() {
-	var server *ghttp.Server
+
 	var ripper *FacebookRipper
 	var token string
 	var userId string
+	var server *ghttp.Server
 
 	BeforeEach(func(){
-		server = ghttp.NewServer()
+		server = NewFakeFacebook()
 		ripper = NewFacebookRipper(server.URL())
 		token = "ValidFacebookToken"
 		userId = "validUserId"
-
-		//setup server
-		server.AppendHandlers(ghttp.CombineHandlers(
-			ghttp.VerifyRequest("GET", "/validUserId/groups"),
-			ghttp.VerifyFormKV("access_token", "ValidFacebookToken"),
-			ghttp.RespondWith(http.StatusOK, "something"),
-		))
 	})
 
-	It("calls the group url", func(){
-		ripper.LookForOrders(userId, token)
-		Expect(len(server.ReceivedRequests())).To(Equal(1))
+	It("gets the list of facebook groups", func(){
+		groups := ripper.GetUsersGroups(userId, token)
+		Expect(len(server.ReceivedRequests())).Should(BeNumerically(">", 0))
+		Expect(server.ReceivedRequests()[0].URL.Path).To(Equal("/validUserId/groups"))
+
+		Expect(groups).ToNot(BeNil())
+		Expect(groups).ToNot(BeEmpty())
+		Expect(groups[0]).ToNot(BeNil())
+		fmt.Println(groups)
 	})
 })
